c8f6c478e3994fdb02a8fa428b2f39cd10e28573
sovationboss <bartholomaeus.philipp@gmail.com>
2024-03-25 11:44:15 +0100

    added funtionality to adjust/create grid by user input

--- repos/etch-a-sketch/script.js
+++ repos/etch-a-sketch/script.js
@@ -1,17 +1,23 @@
-// script.js
-
-const container = document.getElementById('container');
+const container = document.getElementById('drawing-area');
+const createButton = document.getElementById('create-button')
 
 // Create 16x16 square divs
-const createSquareDivs = () => {
+const createSquareDivs = (size) => {
+
+    // clearing (existing) grid
+    container.innerHTML = ' ';
+
+    //calculate flex-basis value for each square
+    const flexBasis = 100 / size + '%';
+
     //iterate over rows
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         //iterate over columns in each row
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement('div');
-            square.classList.add('drawing-area');
+            square.classList.add('square');
             container.appendChild(square);
-            
+            square.style.flexBasis = flexBasis;
             // Event listener for mouseenter event
             square.addEventListener('mouseenter', () => {
                 square.classList.add('hovered');
@@ -25,5 +31,16 @@ const createSquareDivs = () => {
     }
 };
 
-// Call the function to create square divs
+// call the function to create square divs
 createSquareDivs();
+
+//function get the user input
+createButton.addEventListener('click',(event) => {
+    event.preventDefault();
+    const userInput = prompt("Please enter the size of grid you want: ");
+    if(isNaN(userInput) || userInput < 1 || userInput > 100 || Number.isInteger(userInput)) {
+        alert("Please enter a valid number!");
+    } else {
+        createSquareDivs(userInput);
+    }
+});
